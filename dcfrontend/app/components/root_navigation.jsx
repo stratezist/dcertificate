ec1ecf5a607d90e8ba5d874515b50d91659d2fc6
strategist <parrotabhijeet@gmail.com>
2026-08-17 23:27:57 +0530

    Added new UI design for root navigation

--- dcfrontend/app/components/root_navigation.jsx
+++ dcfrontend/app/components/root_navigation.jsx
@@ -13,14 +13,14 @@ export default function RootNavigation() {
     }
 
     function handleAnimationEnd() {
-        if(!open)
+        if (!open)
             setShouldRender(false);
     }
 
-    useEffect(()=>{
-        if(open)
+    useEffect(() => {
+        if (open)
             setShouldRender(true);
-    },[open]);
+    }, [open]);
 
     return <>
         <button className={styles.toggle} onClick={toggle}>
@@ -30,25 +30,30 @@ export default function RootNavigation() {
         {/* Following line is temporary fix for text hiding behind floating button.
             Adds Space at bottom of page if you put this component at bottom of page.
         */}
-        <div style={{height:'100px'}}></div>
+        <div style={{ height: '100px' }}></div>
 
         {shouldRender &&
-            <div className={`${styles.nav_container} ${open?styles.slideIn:styles.slideOut}`} onAnimationEnd={handleAnimationEnd}>
+            <div className={`${styles.nav_container} ${open ? styles.slideIn : styles.slideOut}`} onAnimationEnd={handleAnimationEnd}>
                 <nav className={styles.root_navigation}>
                     {/* <h1>Menu</h1>
                     <hr style={{width:'300px'}} /> */}
-                    <h2>Recipient</h2>
-                    <NavLink className={({ isActive }) => isActive ? styles.active_navlink : styles.navlink} to='/recipient/login'>Login</NavLink>
-                    <NavLink className={({ isActive }) => isActive ? styles.active_navlink : styles.navlink} to='/recipient/register'>Register</NavLink>
-
-                    <h2>Issuer</h2>
-                    <NavLink className={({ isActive }) => isActive ? styles.active_navlink : styles.navlink} to='/issuer/login'>Login</NavLink>
-                    <NavLink className={({ isActive }) => isActive ? styles.active_navlink : styles.navlink} to='/issuer/register'>Register</NavLink>
-
-                    <h2>Other Options</h2>
-                    <NavLink className={({ isActive }) => isActive ? styles.active_navlink : styles.navlink} to='/'>Home</NavLink>
-                    <NavLink className={({ isActive }) => isActive ? styles.active_navlink : styles.navlink} to='/search-certificate'>Search</NavLink>
-                    <NavLink className={({ isActive }) => isActive ? styles.active_navlink : styles.navlink} to='/user-manual'>User Manual</NavLink>
+                    <fieldset className={styles.optionsGroupWithLegend}>
+                        <legend>Recipients</legend>
+                        <NavLink className={({ isActive }) => isActive ? styles.active_navlink : styles.navlink} to='/recipient/login'>Login</NavLink>
+                        <NavLink className={({ isActive }) => isActive ? styles.active_navlink : styles.navlink} to='/recipient/register'>Register</NavLink>
+                    </fieldset>
+                    <fieldset className={styles.optionsGroupWithLegend}>
+                        <legend>Issuers</legend>
+                        <NavLink className={({ isActive }) => isActive ? styles.active_navlink : styles.navlink} to='/issuer/login'>Login</NavLink>
+                        <NavLink className={({ isActive }) => isActive ? styles.active_navlink : styles.navlink} to='/issuer/register'>Register</NavLink>
+                    </fieldset>
+                    <fieldset className={styles.optionsGroup}>
+                        {/* <legend>Other Options</legend> */}
+                        <NavLink className={({ isActive }) => isActive ? styles.active_navlink : styles.navlink} to='/'>Home</NavLink>
+                        <NavLink className={({ isActive }) => isActive ? styles.active_navlink : styles.navlink} to='/search-certificate'>Search</NavLink>
+                        <NavLink className={({ isActive }) => isActive ? styles.active_navlink : styles.navlink} to='/user-manual'>Manual</NavLink> {/*User Manual*/}
+                        <button>Theme</button>
+                    </fieldset>
                 </nav>
             </div>
         }
