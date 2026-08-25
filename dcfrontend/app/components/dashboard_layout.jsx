87068faee763d695b8b00e334d8877fe9eccf79c
strategist <parrotabhijeet@gmail.com>
2026-08-25 22:20:49 +0530

    Little dashboard navbar UI and animation enhancements

--- dcfrontend/app/components/dashboard_layout.jsx
+++ dcfrontend/app/components/dashboard_layout.jsx
@@ -8,7 +8,8 @@ import { X, Menu } from "lucide-react";
 export default function DashboardLayout({ nav_items, title, username, logoutHandlerURL }) {
     const width = useWindowWidth();
     const isMobile = width < 700;
-    const [navbarVisible, setNavBarVisible] = useState(false);
+    const [navbarOpen, setNavBarOpen] = useState(false);
+    const [navShouldRender, setNavShouldRender] = useState(false);
 
     const location = useLocation();
 
@@ -17,10 +18,18 @@ export default function DashboardLayout({ nav_items, title, username, logoutHand
     // on successful navigation to a new page/url.
     // This is a UX enhancement.
     useEffect(function () {
-        setNavBarVisible(false);
+        setNavBarOpen(false);
     }, [location]);
 
-    const navigationBar = <nav className={isMobile ? styles.navbarMobile : styles.navbar}>
+    useEffect(function () {
+        if(navbarOpen) setNavShouldRender(true);
+    }, [navbarOpen]);
+
+    function handleAnimationEnd(){
+        if(!navbarOpen) setNavShouldRender(false);
+    }
+
+    const navigationBar = <nav className={`${isMobile ? styles.navbarMobile : styles.navbar} ${navbarOpen? styles.slideUpAnimation:styles.slideDownAnimation}`} onAnimationEnd={handleAnimationEnd}>
         {title} Account
         <h1>{username}</h1>
         {Object.entries(nav_items).map(
@@ -34,10 +43,10 @@ export default function DashboardLayout({ nav_items, title, username, logoutHand
         {function () {
             if (isMobile)
                 return <>
-                    <button className = {styles.toggleNav} onClick = {function () {setNavBarVisible(!navbarVisible);}}>
-                        {navbarVisible?<X/>:<Menu/>}
+                    <button className = {styles.toggleNav} onClick = {function () {setNavBarOpen(!navbarOpen);}}>
+                        {navbarOpen?<X/>:<Menu/>}
                     </button>
-                    {navbarVisible && navigationBar}
+                    {navShouldRender && navigationBar}
                 </>;
             else
                 return navigationBar;
